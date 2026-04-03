6c7aa6b166a992919561367a98770c4093506b8a
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-03 15:27:27 -0300

    Update msgpack_codec.py

--- 9-protocols/msgpack_codec.py
+++ 9-protocols/msgpack_codec.py
@@ -1,1 +1,13 @@
+import msgpack 
+import io 
+import zlib
+from typing import Any, Dict
 
+class MessagePackCodec:
+    def encode(self, data: Dict[str, Any]) -> bytes:
+        packed = msgpack.packb(data)
+        return zlib.compress(packed)
+        
+    def decode(self, bin_data: bytes) -> Dict[str, Any]:
+        decompressed = zlib.decompress(bin_data)
+        return msgpack.unpackb(decompressed)
